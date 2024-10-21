93e820e18fda8b4258b9baadd387722dfe140ce3
Eduardo Nogueira <eduardodudunog@gmail.com>
2024-10-21 16:23:35 -0300

    refactor: remove envs from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     domains: ["preview.redd.it", "external-preview.redd.it", "styles.redditmedia.com"]
   },
-  env: {
-    APP_URL: process.env.APP_URL,
-    APP_AUTH_URL: process.env.APP_AUTH_URL,
-    REDDIT_CLIENT_ID: process.env.REDDIT_CLIENT_ID,
-    REDDIT_CLIENT_SECRET: process.env.REDDIT_CLIENT_SECRET,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    NEXT_PUBLIC_REDDIT_REDIRECT_URI: process.env.NEXT_PUBLIC_REDDIT_REDIRECT_URI,
-  },
 };
 
 export default nextConfig;
