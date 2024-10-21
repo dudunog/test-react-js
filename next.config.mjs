a70a3209be6f41b9c2a874c3799947e162d57733
Eduardo Nogueira <eduardodudunog@gmail.com>
2024-10-21 15:47:35 -0300

    chore: add envs to next config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   images: {
     domains: ["preview.redd.it", "external-preview.redd.it", "styles.redditmedia.com"]
+  },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_REDDIT_REDIRECT_URI: process.env.NEXT_PUBLIC_REDDIT_REDIRECT_URI,
   }
 };
 
